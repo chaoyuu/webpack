5e6bae324780ce880cef700938addbb9c62fd0e5
chaoyuu <chaoyu995@gmail.com>
2023-08-08 10:15:38 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import count from './js/count';
+import  sum from './js/sum';
+// import css from "file.css";
+import "./css/style.css";
+import "./less/index.less";
+import "./sass/index.sass";
+import "./sass/index.scss";
+import "./stylus/index.styl";
+import "./css/iconfont.css";
+
+console.log(count(2,1));
+
+console.log(sum(1,2,3,4));
